851dff0f92763c3ad5419e831e61baa5cd43872f
wangji <wangji156@>
2020-10-27 00:13:41 +0800

    推送新组件

--- projects/primeng-complex/src/public-api.ts
+++ projects/primeng-complex/src/public-api.ts
@@ -1,10 +1,7 @@
-import { from } from 'rxjs';
-
 /*
  * Public API Surface of primeng-complex
  */
 export * from './lib/primeng-complex.service';
 export * from './lib/primeng-complex.component';
-export * from './lib/primeng-complex.module';
 export * from './lib/primeng-complex.module';
 export * from './tabs';
--- projects/primeng-complex/src/tabs/index.ts
+++ projects/primeng-complex/src/tabs/index.ts
@@ -1,3 +1,4 @@
+export { TabsViewContentDirective } from './tabs-view.directive'
 export {PCTabsModule } from './tabs.module'
 export {PCTabsComponent} from './tabs.component'
 export {PCTabsViewComponent} from './tabs-view.component'
